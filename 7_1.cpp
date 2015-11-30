3bffba682ca170de39af23250ec94d7bc7f0bf0b
anatomasovic <anatomasovic2@gmail.com>
2015-11-30 14:39:28 +0100

    Update 7_1.cpp

--- 7_1.cpp
+++ 7_1.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+class Klasa
+{
+    private:
+        int a;
+    public:
+        Klasa() { a = 5; cout<<"Konstruktor..."<<endl; }
+        ~Klasa() { cout<<"Destruktor..."<<endl; }
+        void setA(int p) { a = p; }
+        int getA() { return a; }
+};
+
+
+void f1(Klasa k1)
+{
+    cout<<"a(f1) = "<<k1.getA()<<endl;
+    k1.setA(10);
+    cout<<"a(f1) = "<<k1.getA()<<endl;
+}
+
+int main()
+{
+    Klasa k;
+    cout<<"Poziv f1 (po vrijednosti): "<<endl;
+    f1(k);
+    cout<<"Vrijednost nakon poziva: "<<k.getA()<<endl;
+    return 0;
+}
 
